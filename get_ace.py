31916fd5cd4ec3c996044bf760d252c93d47343e
Jean Connelly <jconnelly@cfa.harvard.edu>
2014-07-27 16:17:22 -0400

    Warn on ACE, GOES, and HRC proxy if no new data for 20 minutes

--- get_ace.py
+++ get_ace.py
@@ -16,6 +16,17 @@ parser.add_argument('--h5',
                     help='HDF5 file name')
 args = parser.parse_args()
 
+h5 = tables.openFile(args.h5, mode='r',
+                     filters=tables.Filters(complevel=5, complib='zlib'))
+try:
+    table = h5.root.data
+    lasttime = table.col('time')[-1]
+except tables.NoSuchNodeError:
+    lasttime = None
+h5.close()
+
+stale_time = 20  # warn if hasn't been updated in N minutes
+
 url = 'ftp://ftp.swpc.noaa.gov/pub/lists/ace/ace_epam_5m.txt'
 
 colnames = ('year month dom  hhmm  mjd secs p1  p2  p3 '
@@ -29,7 +40,17 @@ for _ in range(3):
     except Exception as err:
         time.sleep(5)
 else:
-    print 'Warning: failed to open URL {}: {}'.format(url, err)
+    # If we don't have any old data to determine "staleness"
+    # raise a warning at this point.
+    if lasttime is None:
+        print 'Warning: failed to open URL {}: {}'.format(url, err)
+    # Otherwise, raise warnings only if data stale more than
+    # stale_time threshold
+    else:
+        if lasttime + (stale_time * 60) < DateTime().secs:
+            how_old = (DateTime().secs - lasttime) / 60.
+            print 'Warning: ACE data stale {}mins'.format(how_old)
+            print 'Warning: failed to open URL {}: {}'.format(url, err)
     sys.exit(0)
 
 colnames = ('year month dom  hhmm  mjd secs '
