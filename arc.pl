5523ac835d3154ce4c4dad0c1b5ea5bcc44250f6
Jean Connelly <jconnelly@cfa.harvard.edu>
2025-03-04 13:38:23 -0500

    Update patch version in arc.pl

--- arc.pl
+++ arc.pl
@@ -33,7 +33,7 @@ use Net::Netrc;
 our $Task     = 'arc3';
 our $TaskData = "$ENV{SKA_DATA}/$Task";
 our $TaskShare = "$ENV{SKA_SHARE}/$Task";
-our $VERSION = '4.10.1';
+our $VERSION = '4.10.2';
 
 require "$ENV{SKA_SHARE}/$Task/Event.pm";
 require "$ENV{SKA_SHARE}/$Task/Snap.pm";
